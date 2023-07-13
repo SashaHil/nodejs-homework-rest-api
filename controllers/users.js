6ea36500a12d6ad7e0f38a7e858fc3438ea74353
SashaHil <sashahil96@gmail.com>
2023-07-13 20:02:27 +0300

    add avatar size

--- controllers/users.js
+++ controllers/users.js
@@ -87,6 +87,8 @@ const updateAvatar = async (req, res) => {
   const { path: tempUpload, originalname } = req.file;
   const filename = `${_id}_${originalname}`;
   const resultUpload = path.join(avatarsDir, filename);
+  const image = await Jimp.read(tempUpload);
+  image.resize(250, 250).write(tempUpload);
   await fs.rename(tempUpload, resultUpload);
   const avatarURL = path.join("avatars", filename);
   await User.findByIdAndUpdate(_id, { avatarURL });
