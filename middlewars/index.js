762bdf8cdfe6e7fc9e003906d5b84fa28596d594
SashaHil <sashahil96@gmail.com>
2023-07-13 19:59:20 +0300

    adding avatar

--- middlewars/index.js
+++ middlewars/index.js
@@ -1,9 +1,11 @@
 const validateBody = require("./validateBody");
 const isValidtId = require("./isValidId");
 const authenticate = require("./authenticate");
+const upload = require("./upload");
 
 module.exports = {
   validateBody,
   isValidtId,
   authenticate,
+  upload,
 };
